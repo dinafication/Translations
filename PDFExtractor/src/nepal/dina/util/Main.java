9a2779eea982b2ecaad67dbb9d5a9070f585d409
dinafication <dinafication@gmail.com>
2013-02-16 14:42:54 +0100

    util nije bio oznacen za version control

--- PDFExtractor/src/nepal/dina/util/Main.java
+++ PDFExtractor/src/nepal/dina/util/Main.java
@@ -1,4 +1,5 @@
 package nepal.dina.util;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -33,84 +34,83 @@ import org.xml.sax.SAXException;
 
 import com.memetix.mst.language.Language;
 
-
 public class Main {
 
 	/**
 	 * @param args
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
 
-		
 		try {
-			
-//			HashSet<String> ret = new HashSet<String>();
-//			
-//			Parser parser = new TotalnEnglishElementary("res\\levels\\I\\Total_English_Intermediate_Wordlist_English_-_Dutch_-_French.pdf");
-//			ArrayList<String> ret1 = parser.extract(15,2);
-//			parser.close();
-//			ret.addAll(ret1);
-			
-//			Parser parser2 = new NewEnglisFilePreIntermediateWordlist("res\\levels\\PI\\prevedno\\New English File Pre-Intermediate Wordlist.pdf");
-//			ArrayList<String> ret2 = parser2.extract(7,2);
-//			parser.close();
-//			ret.addAll(ret2);
-			
-//			Parser parser3 = new total_english_pre_intermediate_eng_fra("res\\levels\\PI\\prevedno\\total-english-pre-intermediate-eng-fra.pdf");
-//			ArrayList<String> ret3 = parser3.extract(5,2);
-//			parser3.close(); 
-//			ret.addAll(ret3);
-//			
-			
-			
+
+			// HashSet<String> ret = new HashSet<String>();
+			//
+			// Parser parser = new
+			// TotalnEnglishElementary("res\\levels\\I\\Total_English_Intermediate_Wordlist_English_-_Dutch_-_French.pdf");
+			// ArrayList<String> ret1 = parser.extract(15,2);
+			// parser.close();
+			// ret.addAll(ret1);
+
+			// Parser parser2 = new
+			// NewEnglisFilePreIntermediateWordlist("res\\levels\\PI\\prevedno\\New English File Pre-Intermediate Wordlist.pdf");
+			// ArrayList<String> ret2 = parser2.extract(7,2);
+			// parser.close();
+			// ret.addAll(ret2);
+
+			// Parser parser3 = new
+			// total_english_pre_intermediate_eng_fra("res\\levels\\PI\\prevedno\\total-english-pre-intermediate-eng-fra.pdf");
+			// ArrayList<String> ret3 = parser3.extract(5,2);
+			// parser3.close();
+			// ret.addAll(ret3);
+			//
+
 			/**
 			 * 1. Upisi extrahirane, neprevedene rijeci u file.
 			 * */
-		//FileIO.writeInFile("res\\levels\\I\\I", ret);
-			
-			
+			// FileIO.writeInFile("res\\levels\\I\\I", ret);
+
 			/**
-			 * 2. Raspodijeli file u vise filova, tako da imaju manje od 500 entrija.
+			 * 2. Raspodijeli file u vise filova, tako da imaju manje od 500
+			 * entrija.
 			 * */
-			
-			
+
 			/**
 			 * 3. Ucitaj rijeci iz fila.
 			 * */
-			HashSet<String> words = FileIO.readFromFile("res\\wordlists\\I\\I_8");
-			//HashSet<String> words = FileIO.readFromFile("res\\levels\\PI\\PI_1");
-			
-			
+			HashSet<String> words = FileIO
+					.readFromFile("res\\wordlists\\I\\I_8");
+			// HashSet<String> words =
+			// FileIO.readFromFile("res\\levels\\PI\\PI_1");
+
 			/**
 			 * 4. Prevedi.
 			 * */
 			Language from = Language.ENGLISH;
 			Language dest = Language.ITALIAN;
 			GlodsyTranslator gt = new GlodsyTranslator(from, dest);
-			HashMap<String, HashSet<String>> wordsNTranslations = gt.translateAll(words, "ita");
+			HashMap<String, HashSet<String>> wordsNTranslations = gt
+					.translateAll(words, "ita");
 			JsonIO jio = new JsonIO();
-			String jios = null;			
-			
+			String jios = null;
+
 			WordTranslationList list = new WordTranslationList();
-			for(String word:wordsNTranslations.keySet()){
-				list.addWT(new WordTranslation(word, wordsNTranslations.get(word)));
+			for (String word : wordsNTranslations.keySet()) {
+				list.addWT(new WordTranslation(word, wordsNTranslations
+						.get(word)));
 			}
-			
+
 			jios = jio.getString(list);
-			
-			
+
 			/**
 			 * 5. Upisi u file.
 			 * */
-		FileIO.appendFile("res\\translations\\talian\\I\\I_eng_tal_8", jios);
-			
-		}
-		catch (IOException e) {
-			 
+			FileIO.appendFile("res\\translations\\talian\\I\\I_eng_tal_8", jios);
+
+		} catch (IOException e) {
+
 			e.printStackTrace();
 		}
-		
 
 	}
 
